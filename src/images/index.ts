e428bed1147d1bc75285121eb32ef9bf8c20d1e3
Renny Suarez <rennyarmando@gmail.com>
2024-05-31 06:14:36 -0400

    Feat: Hero section and tailwind installed

---
+++ src/images/index.ts
@@ -0,0 +1,11 @@
+import LocalizationIcon from "./icons/localization.astro"
+import MailIcon from "./icons/mail.astro"
+import PhoneIcon from "./icons/phone.astro"
+import GitHubLogo from "./logos/github.astro"
+
+export {
+    GitHubLogo,
+    LocalizationIcon,
+    MailIcon,
+    PhoneIcon,
+}
