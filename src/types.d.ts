361f7f76ffb7bece25745c01a01c2c012da8dae9
Renny Suarez <rennyarmando@gmail.com>
2024-06-01 11:09:47 -0400

    Feat: Education section

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,35 +1,47 @@
 export interface CV {
-  name: string;
-  avatar: string;
-  about: string;
-  sumary: string;
-  location: Location;
-  contact: Contact;
-  work: Work[];
+    name: string;
+    avatar: string;
+    about: string;
+    sumary: string;
+    location: Location;
+    contact: Contact;
+    work: Work[];
+    education: Education[];
 }
 
 interface Work {
-  name: string;
-  position: string;
-  url?: string;
-  startDate: string;
-  endDate: null | string;
-  summary: string;
-  highlights: string[];
+    name: string;
+    position: string;
+    url?: string;
+    startDate: string;
+    endDate: null | string;
+    summary: string;
+    highlights: string[];
 }
 
 interface Contact {
-  email: string;
-  tel: string;
-  socials: Social[];
+    email: string;
+    tel: string;
+    socials: Social[];
 }
 
 interface Social {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
 }
 
 interface Location {
-  link: string;
-  text: string;
+    link: string;
+    text: string;
+}
+
+interface Education {
+    institution: string;
+    url: string;
+    area: string;
+    studyType: string;
+    startDate: string;
+    endDate: string;
+    score: string;
+    courses: string[];
 }
