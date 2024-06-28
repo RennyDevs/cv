b08fbecb9b1490029ed68028d6cde8143c83b296
Renny Suarez <rennyarmando@gmail.com>
2024-06-28 18:30:56 -0400

    Feat: Education section and card detailed component

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,28 +2,17 @@ export interface CV {
     about: string;
     avatar: string;
     contact: Contact;
-    education: Education[];
+    educations: Education[];
     experiences: Experience[];
     location: Location;
     name: string;
     sumary: string;
-    work: Work[];
-}
-
-interface Work {
-    name: string;
-    position: string;
-    url?: string;
-    startDate: string;
-    endDate: null | string;
-    summary: string;
-    highlights: string[];
 }
 
 interface Contact {
     email: string;
-    tel: string;
     socials: Social[];
+    tel: string;
 }
 
 interface Social {
@@ -36,23 +25,23 @@ interface Location {
     text: string;
 }
 
-interface Education {
-    institution: string;
-    url: string;
+export interface Education {
     area: string;
-    studyType: string;
-    startDate: string;
+    courses?: string[];
     endDate: string;
-    score: string;
-    courses: string[];
+    institution: string;
+    score?: string;
+    startDate: string;
+    studyType?: string;
+    url: string;
 }
 
-export interface Experience {
-  endDate?: string;
-  highlights: string[];
-  name: string;
-  position: string;
-  startDate: string;
-  summary: string;
-  url?: string;
+interface Experience {
+    endDate?: string;
+    highlights: string[];
+    name: string;
+    position: string;
+    startDate: string;
+    summary: string;
+    url?: string;
 }
