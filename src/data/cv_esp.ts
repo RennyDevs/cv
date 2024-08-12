9156e16f2f68ef5d1b3050592aeeec74028cf586
Renny Suarez <rennyarmando@gmail.com>
2024-08-12 09:57:15 -0400

    Refactor: Data assigned

---
+++ src/data/cv_esp.ts
@@ -0,0 +1,23 @@
+
+import type { CV } from "../types";
+
+const data: CV = {
+    "name": "",
+    "avatar": "",
+    "about": "",
+    "sumary": "",
+    "location": {
+        "link": "",
+        "text": ""
+    },
+    "contact": {
+        "email": "",
+        "tel": "",
+        "socials": []
+    },
+    "experiences": [],
+    "educations": [],
+    "skills": [],
+}
+
+export default data
